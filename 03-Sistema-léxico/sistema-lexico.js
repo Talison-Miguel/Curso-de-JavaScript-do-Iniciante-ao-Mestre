4126be53b8f4b4ae76209ff6a24fca23709ce3f3
Tálison Miguel <talisonmiguel84@gmail.com>
2022-02-09 16:21:32 -0300

    Adicionando arquivos da seção do Sistema Léxico

---
+++ 03-Sistema-léxico/sistema-lexico.js
@@ -0,0 +1,11 @@
+/*
+let nome = "Daniel"
+console.log(10 + 20)
+*/
+
+//Uma unica linha
+
+let nome = "Daniel"
+console.log(nome)
+
+let hasAttribite = ""
---
+++ 03-Sistema-léxico/use-strict.js
@@ -0,0 +1,30 @@
+"use strict"
+// c = 10
+
+// function foo(){
+//     "use strict"
+//     let x = 20
+// }
+
+// foo()
+
+// console.log(c)
+
+// function dobrar(n1, n1){
+//     "use strict"
+//     console.log(n1, n1)
+//     return n1 * n1
+// }
+// console.log(dobrar(5))
+
+// function Teste(){
+//     "use strict"
+//     console.log(this)
+//     this.a = "a"
+// }
+
+// Teste()
+
+let msg = "uma string"
+msg.count = 0
+console.log(msg)
