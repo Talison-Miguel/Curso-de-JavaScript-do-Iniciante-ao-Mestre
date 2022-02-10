1a9d9958f7e28ab1ad807b2730df2563bef04607
Tálison Miguel <talisonmiguel84@gmail.com>
2022-02-10 13:17:12 -0300

    Adicionando seçao de funçoes

---
+++ 04-funções/arguments.js
@@ -0,0 +1,11 @@
+function somar(){
+    console.log(arguments)
+    let total = 0
+    for(let i = 0; i < arguments.length; i++){
+        total += arguments[i]
+    }
+    return total
+}
+
+console.log(somar(1, 2, 3))
+console.log(somar(1, 2, 4, 5 ,6))
---
+++ 04-funções/iife-menu.js
@@ -0,0 +1,19 @@
+
+// function init(){
+//     let isValid = false
+//     console.log('init menu', isValid)
+// }
+// init()
+
+(function(win, doc){
+    "use strict"
+    let isValid = false
+    // win.alert("ola mundo")
+    console.log('menu', isValid)   
+
+    function init(){
+        console.log("init do menu")
+    }
+    init()
+})(window, document)
+
