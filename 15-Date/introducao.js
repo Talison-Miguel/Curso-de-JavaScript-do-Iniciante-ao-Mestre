55cce2905223c1e9fdd8ec1515f85354cd32ae5a
Tálison Miguel <talisonmiguel84@gmail.com>
2022-08-01 18:50:59 -0300

    Seçao Date

---
+++ 15-Date/introducao.js
@@ -0,0 +1,14 @@
+const data = new Date()
+console.log(data)
+console.log(data.getDay()) // Dia da semana
+console.log(data.getDate()) //Dia do mes
+console.log(data.getMonth()) // O mes do ano, si qye começa a contar no 0, por isso da um numero a meno
+console.log(data.getTime()) //Numero de milissegundos desde 1970
+console.log(data.getFullYear()) //O ano
+console.log(data.getHours()) //A hora do dia
+console.log(data.getUTCHours()) //A hora universal
+
+
+
+
+
