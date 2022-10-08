7779e484287923d7112def1d74a53b9b72c416c8
Tálison Miguel <talisonmiguel84@gmail.com>
2022-10-08 08:35:01 -0300

    Exercicio bg-paralax

---
+++ 18-Estilização/bg_paralax_original/js/paralax.js
@@ -0,0 +1,33 @@
+
+
+const dataParalaxContainer = [...document.querySelectorAll('[data-paralax]')]
+
+function isGettingOut(container) {
+    return container.getBoundingClientRect().top <= 0
+}
+
+function getNewPosition(c) {
+    const v = parseFloat(c.getAttribute("data-p-velocity")) || .5
+
+    return c.getBoundingClientRect().top * v * -1
+}
+
+function positionImage() {
+    dataParalaxContainer.forEach(c => {
+        let originalPositionY = getComputedStyle(c).backgroundPositionY
+        let originalPositionX = getComputedStyle(c).backgroundPositionX
+
+        console.log(originalPositionY)
+        console.log(originalPositionX)
+
+        if(isGettingOut(c)) {
+            c.style.backgroundPosition = ` ${originalPositionX} ${getNewPosition(c)}px `
+        }else {
+            c.style.backgroundPosition = ` ${originalPositionX} 0px `
+        }
+    })
+}
+
+window.addEventListener('scroll', positionImage)
+
+positionImage()
