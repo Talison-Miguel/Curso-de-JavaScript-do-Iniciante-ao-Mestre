fe2541e58a26dd2c81b8079267ee03f211e85d3f
Tálison Miguel <talisonmiguel84@gmail.com>
2022-11-25 18:14:08 -0300

    destructoring

---
+++ 21-Orientação-a-Objetos-Parte-2/destructoring.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const eu = { nome: 'talison', sobrenome: 'miguel' }
+
+//extraindo as propiedades
+const { nome: talis, sobrenome } = eu
+
+console.log(talis)
+console.log(sobrenome)
