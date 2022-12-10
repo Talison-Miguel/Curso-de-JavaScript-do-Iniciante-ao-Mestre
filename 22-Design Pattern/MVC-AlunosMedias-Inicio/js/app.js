226feffc8af1d1d06f353a218c4dca1431c7cf5f
Tálison Miguel <talisonmiguel84@gmail.com>
2022-12-10 18:22:54 -0300

    localStorage

--- 22-Design Pattern/MVC-AlunosMedias-Inicio/js/app.js
+++ 22-Design Pattern/MVC-AlunosMedias-Inicio/js/app.js
@@ -1,51 +1,48 @@
-const alunos = [
-    {
-        _id: 0,
-        nome: "chico melato",
-        notas: {
-            portugues: [1, 1, 2, 2],
-            matematica: [2, 2, 2, 2],
-            historia: [2, 2, 3, 3],
-            ciencias: [3, 3, 3, 3],
-        },
-    },
-    {
-        _id: 1,
-        nome: "talita lima",
-        notas: {
-            portugues: [4, 4, 4, 4],
-            matematica: [4, 4, 5, 5],
-            historia: [5, 5, 6, 6],
-            ciencias: [7, 7, 8, 9],
-        },
-    },
-    {
-        _id: 2,
-        nome: "Tálison",
-        notas: {
-            portugues: [9, 9, 9, 9],
-            matematica: [10, 9, 8, 9],
-            historia: [8, 9, 9, 6],
-            ciencias: [10, 7, 8, 9],
-        },
-    },
-];
+// const alunos = [
+//     {
+//         _id: 0,
+//         nome: "chico melato",
+//         notas: {
+//             portugues: [1, 1, 2, 2],
+//             matematica: [2, 2, 2, 2],
+//             historia: [2, 2, 3, 3],
+//             ciencias: [3, 3, 3, 3],
+//         },
+//     },
+//     {
+//         _id: 1,
+//         nome: "talita lima",
+//         notas: {
+//             portugues: [4, 4, 4, 4],
+//             matematica: [4, 4, 5, 5],
+//             historia: [5, 5, 6, 6],
+//             ciencias: [7, 7, 8, 9],
+//         },
+//     },
+//     {
+//         _id: 2,
+//         nome: "Tálison",
+//         notas: {
+//             portugues: [9, 9, 9, 9],
+//             matematica: [10, 9, 8, 9],
+//             historia: [8, 9, 9, 6],
+//             ciencias: [10, 7, 8, 9],
+//         },
+//     },
+// ];
 
 
 const alunosService = new AlunosService()
 
-//Calcula a media por propiedade de cada aluno e cria uma propiedade chamda media
-alunos.forEach( aluno => {
-    alunosService.add(new AlunoModel(aluno))
-} )
+// alunos.forEach( aluno => {
+//     alunosService.add(new AlunoModel(aluno))
+// } )
 
 
 const alunosView = new AlunosView(document.querySelector('[data-table-alunos]'))
 
 
 const alunosControler = new AlunosControler(alunosService, alunosView)
-
-
 
 
 
