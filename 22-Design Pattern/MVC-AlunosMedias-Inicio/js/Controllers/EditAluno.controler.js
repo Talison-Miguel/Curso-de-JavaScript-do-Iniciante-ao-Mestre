46702d90869baf49c68229198f184e09206da1f4
Tálison Miguel <talisonmiguel84@gmail.com>
2022-12-20 09:04:03 -0300

    Update edit alunos

--- 22-Design Pattern/MVC-AlunosMedias-Inicio/js/Controllers/EditAluno.controler.js
+++ 22-Design Pattern/MVC-AlunosMedias-Inicio/js/Controllers/EditAluno.controler.js
@@ -1,8 +1,28 @@
 //faz a ligaçao entre os services e a view para renderizar
 class EditAlunoCrontroller {
-    constructor(model, view) {
+    constructor(model, view, service) {
         this.model = model;
         this.view = view;
+        this.service = service
         this.view.render(model)
+    }
+
+    edit(aluno, nome) {
+        aluno.nome = nome
+
+        // {
+        //     _id: 0,
+        //     nome: "chico melato",
+        //     notas: {
+        //         portugues: [1, 1, 2, 2],
+        //         matematica: [2, 2, 2, 2],
+        //         historia: [2, 2, 3, 3],
+        //         ciencias: [3, 3, 3, 3],
+        //     },
+        // }
+
+        const notas = {}
+
+        this.service.edit(aluno, notas)
     }
 }
