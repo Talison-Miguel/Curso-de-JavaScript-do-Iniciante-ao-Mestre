624263b64d0232eacf943df85f913a50a8326ac9
Tálison Miguel <talisonmiguel84@gmail.com>
2023-01-17 13:19:56 -0300

    common js

---
+++ 26-CommonJs/modules/mod1.js
@@ -0,0 +1,6 @@
+// module.exports.teste = 'ola mundo'
+exports.fn = () => {
+    console.log("funçao em module.exports")
+    return "--- funçao em module.exports ---"
+}
+
---
+++ 26-CommonJs/modules/mod2.js
@@ -0,0 +1,1 @@
+module.exports.teste = "uma string exportada"
