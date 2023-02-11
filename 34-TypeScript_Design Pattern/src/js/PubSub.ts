60dffb9200959799df607d6ba07fd746dc8ad081
Tálison Miguel <talisonmiguel84@gmail.com>
2023-02-11 15:30:21 -0300

    continuaçao

--- 34-TypeScript_Design Pattern/src/js/PubSub.ts
+++ 34-TypeScript_Design Pattern/src/js/PubSub.ts
@@ -1,24 +1,42 @@
-export class PubSub {
-    static subscribers = {}
+type CallBackProps= (data?: OutputProps) => void
 
-    static subscriber(eventName: string, callback: Function) {
+export interface OutputProps  {
+    data?: any,
+    type: string
+}
+
+type SubscribersProps =  {
+    [Prop in string]: CallBackProps[]
+}
+
+export class PubSub {
+    static subscribers: SubscribersProps = {}
+
+    static subscriber(eventName: string, callback: CallBackProps) {
         PubSub.subscribers[eventName] = PubSub.subscribers[eventName] || []
         PubSub.subscribers[eventName].push(callback)
+
+        PubSub.subscribers.teste = []
     }
 
-    static unsubscriber(eventName: string, callback: Function) {
+    static unsubscriber(eventName: string, callback: CallBackProps) {
         if(PubSub.subscribers[eventName]) {
             const index = PubSub.subscribers[eventName].findIndex((element: Function) => element === callback)
             if(index > -1) {
                 PubSub.subscribers[eventName].splice(index, 1)
+
+                if(PubSub.subscribers[eventName].length === 0) {
+                    delete PubSub.subscribers[eventName]
+                }
             }
         }
     }
 
     static publish(eventName: string, data: any) {
         if(PubSub.subscribers[eventName]) {
-            PubSub.subscribers[eventName].forEach( (fn: Function) => {
-                fn(data)
+            PubSub.subscribers[eventName].forEach( (fn: CallBackProps) => {
+                const output: OutputProps = {data, type: eventName}
+                fn(output)
             });
         }
     }
